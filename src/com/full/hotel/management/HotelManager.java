78b78aba7ec1b0d74a1eb3d9c0f8110dbb0e773a
Yogeswari A <yogeshwari.arjunan@adaptavantcloud.com>
2017-06-03 07:22:04 +0530

    Adding map instead of list

--- src/com/full/hotel/management/HotelManager.java
+++ src/com/full/hotel/management/HotelManager.java
@@ -1,57 +1,103 @@
 package com.full.hotel.management;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class HotelManager {
 
+	/*
+	 * private static enum Hotel { PARK(90, 20, 5, 2), HYAAT(82, 12, 5, 3),
+	 * RADDISSON(30, 18, 3, 3), RAINTREE(58, 15, 3, 2), ACCORD(41.55, 11.8, 1,
+	 * 3), FORTUNE(43, 14.8, 1, 4);
+	 * 
+	 * private double costPerDay; private double discountPercentage; private int
+	 * discountDays; private int star;
+	 * 
+	 * private Hotel(double costPerDay, double discountPercentage, int star, int
+	 * discountDays) { this.costPerDay = costPerDay; this.discountDays =
+	 * discountDays; this.discountPercentage = discountPercentage; this.star =
+	 * star;
+	 * 
+	 * }
+	 * 
+	 * 
+	 * 
+	 * public Double getCostPerDay() { return costPerDay; }
+	 * 
+	 * public double getDiscountPercentage() { return discountPercentage; }
+	 * 
+	 * 
+	 * public int getDiscountDays() { return discountDays; }
+	 * 
+	 * public int getStar() { return star; }
+	 * 
+	 * 
+	 * }
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Hotel> hotels = new ArrayList<>();
+		Map<Integer, List<Hotel>> hotelsMap = new HashMap<>();
+
 		try (Scanner scanner = new Scanner(System.in)) {
+			
+
+			/*hotels.add(new Hotel("Park", 90, 20, 5, 2));
+			hotels.add(new Hotel("Hyaat", 82, 12, 5, 3));
+			hotels.add(new Hotel("Raddisson", 30, 18, 3, 3));
+			hotels.add(new Hotel("RainTree", 58, 15, 3, 2));
+			hotels.add(new Hotel("Accord", 41.55, 11.8, 1, 3));
+			hotels.add(new Hotel("Fortune", 43, 14.8, 1, 4));*/
+
+			List<Hotel> fiveStarHotel = new ArrayList<>();
+			fiveStarHotel.add(new Hotel("Park", 90, 20, 5, 2));
+			fiveStarHotel.add(new Hotel("Hyaat", 82, 12, 5, 3));
+			hotelsMap.put(5, fiveStarHotel);
+			
+			List<Hotel> treeStarHotel = new ArrayList<>();
+			treeStarHotel.add(new Hotel("Raddisson", 30, 18, 3, 3));
+			treeStarHotel.add(new Hotel("RainTree", 58, 15, 3, 2));
+			hotelsMap.put(3, treeStarHotel);
+
+			List<Hotel> oneStarHotel = new ArrayList<>();
+			oneStarHotel.add(new Hotel("Accord", 41.55, 11.8, 1, 3));
+			oneStarHotel.add(new Hotel("Fortune", 43, 14.8, 1, 4));
+			hotelsMap.put(1,oneStarHotel);
+
+			
 			System.out.println("Enter amount: ");
 			int amount = scanner.nextInt();
 			System.out.println("How many days u want to stay here?: ");
 			int days = scanner.nextInt();
 			System.out.println("What is your rating 1 or 3 or 5?.");
 			int rating = scanner.nextInt();
-
-			hotels.add(new Hotel("Park", 90, 20, 5, 2));
-			hotels.add(new Hotel("Hyaat", 82, 12, 5, 3));
-			hotels.add(new Hotel("Raddisson", 30, 18, 3, 3));
-			hotels.add(new Hotel("RainTree", 58, 15, 3, 2));
-			hotels.add(new Hotel("Accord", 41.55, 11.8, 1, 3));
-			hotels.add(new Hotel("Fortune", 43, 14.8, 1, 4));
-
+			
+			List<Hotel> hotels=hotelsMap.get(rating);
+			
 			System.out.println(bookHotel(amount, days, rating, hotels));
 		}
 	}
 
 	private static String bookHotel(double money, int days, int starRating, List<Hotel> hotels) {
 
-		double minCost = 0;
+		double cheapestCost = 0;
 		String hotelName = "";
 
 		for (Hotel hotel : hotels) {
-			if (starRating == hotel.getStar()) {
-
-				double totalCost = getCostOfHotel(days, hotel);
-				if (minCost == 0) {
-					minCost = totalCost;
+				double costOfHotel = getCostOfHotel(days, hotel);
+				if (cheapestCost == 0) {
+					cheapestCost = costOfHotel;
 					hotelName = hotel.getHotelName();
 
-				} else if (totalCost < minCost) {
+				} else if (costOfHotel < cheapestCost) {
 					hotelName = hotel.getHotelName();
-					minCost = totalCost;
+					cheapestCost = costOfHotel;
 				}
-
-			}
-
 		}
-
-		return isHotelBooked(money, days, starRating, minCost, hotelName);
+		return isHotelBooked(money, days, starRating, cheapestCost, hotelName);
 
 	}
 
